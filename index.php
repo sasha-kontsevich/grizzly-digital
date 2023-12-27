3b8fa63cda21510b25cd04f59ae66ed7319b9cd0
sasha-kontsevich <sasha-kontsevich@gmail.com>
2023-12-27 23:03:36 +0300

    task3

--- index.php
+++ index.php
@@ -13,6 +13,10 @@
         crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
     <title>Grizzly test</title>
 </head>
@@ -39,9 +43,35 @@ include_once "phone-checker.php";
 
     <button id="clear-cookies-button">Clear cookies</button>
     <div class="cookies-popup hidden" id="cookies-popup">
-            <span>This site use cookies</span>
-            <button id="accept-cookies-button">Accept</button>
-            <button id="decline-cookies-button">Decline</button>
+        <span>This site use cookies</span>
+        <button id="accept-cookies-button">Accept</button>
+        <button id="decline-cookies-button">Decline</button>
+    </div>
+
+    <h2>Задание 3</h2>
+
+    <div class="container-fluid section-bg">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 col-lg-6 panel">
+                    <h1>Оперативная доставка на маркетплейсы</h1>
+                    <ul>
+                        <li>Работаем без выходных 7 дней в неделю</li>
+                        <li>Доставляем "сегодня на сегодня"</li>
+                        <li>Заберём и доставим день в день ваш товар до склада ВБ в Минске</li>
+                        <li>Доставим ваш товар из Минска в Коледино (Россия) по схемам FBO или FBS</li>
+                    </ul>
+                    <button>Подробнее</button>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid  bg-img-r">
+            <div class="row">
+                <div class="col-lg-7 offset-lg-5  col-md-12">
+                    <div class="bg-img"> </div>
+                </div>
+            </div>
+        </div>
     </div>
 </body>
 <script src="cookies.js"></script>
